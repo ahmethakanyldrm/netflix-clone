4c1ffa6c7ce2f1c43f6b30b6e5921efc4fc3f305
ahmethakanyldrm <hakan-336-ahmet@outlook.com>
2024-04-22 00:26:52 +0300

    logo header kısmına yerleştirildi. Background image yerleştirildi ve login ekran tasarımı yapıldı

---
+++ src/app/constants/config.ts
@@ -0,0 +1,2 @@
+export const LOGO_URL = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const BG_IMG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/9f46b569-aff7-4975-9b8e-3212e4637f16/bfcd49ad-2a04-4776-9872-3e9039a1bee5/TR-tr-20240415-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { BG_IMG_URL, LOGO_URL } from '../../constants/config';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+logoUrl = LOGO_URL;
+ bgUrl = BG_IMG_URL;
 }
