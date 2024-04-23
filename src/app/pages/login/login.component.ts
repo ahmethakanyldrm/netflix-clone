edf7a43b506004556743c7e74b9ac9d429376bbc
ahmethakanyldrm <hakan-336-ahmet@outlook.com>
2024-04-23 21:47:10 +0300

    loginService yazıldı.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BG_IMG_URL, LOGO_URL } from '../../constants/config';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,4 +15,25 @@ import { FormsModule } from '@angular/forms';
 export class LoginComponent {
 logoUrl = LOGO_URL;
  bgUrl = BG_IMG_URL;
+
+ email!: string;
+ password!: string;
+ loginService = inject(LoginService);
+ router = inject(Router);
+
+ onSubmit() {
+  //console.log(this.email, this.password);  
+  // validate email and password
+  if(!this.email || !this.password) {
+    alert("provide email or password");
+    return;
+  }
+
+  // if email and password is correct lets login the user
+ this.loginService.login(this.email, this.password);
+ 
+ // redirect To browse page
+ this.router.navigateByUrl("/browse");
+
+ }
 }
